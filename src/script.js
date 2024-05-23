d6dc612154762bc6f9d46f4d8aaa560502f4f3f7
vini <vcassere@gmail.com>
2024-05-23 20:57:20 -0300

    projeto pronto para prod

---
+++ src/script.js
@@ -0,0 +1,145 @@
+function addButton() {
+    // Obtém todos os elementos com a classe "add-button"
+    let buttons = document.querySelectorAll(".add-button");
+    
+    // Para cada botão encontrado
+    buttons.forEach(button => {
+        // Adiciona um ouvinte de evento de clique
+        button.addEventListener("click", function() {
+            // Obtém o próximo elemento irmão com a classe "add-menu"
+            let menu = this.nextElementSibling;
+            
+            // Alterna a exibição do menu
+            if (menu.style.display === "block") {
+                menu.style.display = "none";
+            } else {
+                menu.style.display = "block";
+            }
+        });
+    });
+}
+
+window.onload = addButton;
+
+clearInput = () => {
+
+let input = document.getElementById("insert-link");
+
+    input.value = "";
+
+}
+
+
+addVideoYt = (e, videoInput, videoIframe) => {
+    let input = e.target.parentNode.querySelector("." + videoInput);
+    let link = input.value;
+    let iframe = e.target.parentNode.parentNode.querySelector("." + videoIframe);
+    
+    if(e.keyCode === 13){
+        var video_id = link.split('v=')[1];
+        var ampersandPosition = video_id.indexOf('&');
+        if(ampersandPosition != -1) {
+            video_id = video_id.substring(0, ampersandPosition);
+        }
+
+        var iframe_url = "https://www.youtube.com/embed/" + video_id;
+        iframe.src = iframe_url;
+    }
+    clearInput();
+}
+
+addVideoSpalla = (e,videoInput,videoIframe) => {
+    let input = e.target.parentNode.querySelector("." + videoInput);
+    let link = input.value;
+    let iframe = e.target.parentNode.parentNode.querySelector("." + videoIframe);
+    
+    if(e.keyCode === 13){
+        var video_id = link.split('?live=')[1];
+        var iframe_url = "https://beyond.spalla.io/player/?live=" + video_id;
+        iframe.src = iframe_url;
+    }
+    clearInput();
+}
+
+addVideo = (e, videoInput, videoIframe) => {
+    let inputElement = e.target.parentNode.querySelector("." + videoInput);
+    
+    if (inputElement) {
+        let input = inputElement.value;
+
+        if(input.includes("spalla")){
+            return addVideoSpalla(e, videoInput, videoIframe);
+        }
+        else if (input.includes("youtube")){
+            return addVideoYt(e, videoInput, videoIframe);
+        }
+    } else {
+        console.error('Elemento de entrada não encontrado');
+    }
+
+}
+
+
+// Função para adicionar a tela
+function addScreen() {
+    // Seleciona o elemento div com a classe 'container'
+    let container = document.querySelector('.container');
+
+    // Cria o elemento div com a classe 'tela'
+    let tela = document.createElement('div');
+    tela.className = 'tela';
+
+    // Cria o elemento iframe com a classe 'videoIframe'
+    let iframe = document.createElement('iframe');
+    iframe.className = 'videoIframe';
+    iframe.width = '100%';
+    iframe.height = '100%';
+    iframe.title = 'YouTube video player';
+    iframe.frameborder = '0';
+    iframe.allow = 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share';
+    iframe.referrerpolicy = 'strict-origin-when-cross-origin';
+    iframe.allowfullscreen = true;
+
+    // Cria o elemento button com a classe 'add-button'
+    let button = document.createElement('button');
+    button.className = 'add-button';
+    button.onclick = function() { addButton(this); };
+    button.textContent = '+';
+
+    // Cria o elemento span com a classe 'add-menu'
+    let span = document.createElement('span');
+    span.className = 'add-menu';
+
+    // Cria o elemento input com a classe 'videoInput'
+    let input = document.createElement('input');
+    input.type = 'text';
+    input.placeholder = 'Digite o link';
+    input.className = 'videoInput';
+    input.onkeydown = function(event) { addVideo(event, 'videoInput', 'videoIframe'); };
+
+    // Adiciona os elementos criados ao span
+    span.appendChild(input);
+
+    // Adiciona os elementos criados à div 'tela'
+    tela.appendChild(iframe);
+    tela.appendChild(button);
+    tela.appendChild(span);
+
+    // Adiciona a div 'tela' ao elemento 'container'
+    container.appendChild(tela);
+}
+
+// Seleciona o botão
+let createPlayerButton = document.querySelector('.createPlayer')
+
+// Adiciona o ouvinte de eventos
+createPlayerButton.addEventListener("click", addScreen);
+
+
+
+
+
+
+
+
+
